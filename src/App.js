93639cb692a8218aa34b9aa7f41291ad7d8d9598
Kim Leung <71901468+devworkerkim@users.noreply.github.com>
2021-10-10 20:40:14 -0600

    Convert class components to function components
    refactor!: convert class components to function components
    docs: update README.md

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import Info from './components/Info';
 import Education from './components/Education';
 import Experience from './components/Experience';
 
-class App extends React.Component {
-  render () {
-    return (
-      <div className='App'>
-        <h1>CV Application | The Odin Project</h1>
-        <Info />
-        <Education />
-        <Experience />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className='App'>
+      <h1>CV Application | The Odin Project</h1>
+      <Info />
+      <Education />
+      <Experience />
+    </div>
+  )
 }
 
 export default App;
